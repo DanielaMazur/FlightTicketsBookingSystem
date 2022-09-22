dd5d83acc7e8dd4f49765ca8d13fa486191924c9
Osmatescu Antonina <aosmatescu@gmail.com>
2022-09-22 12:08:06 +0300

    Add AuthService

---
+++ AuthService/AuthService/Config.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace AuthService
+{
+     public class Config
+     {
+          public static IEnumerable<ApiScope> ApiScopes =>
+               new List<ApiScope>
+               {
+                    new ApiScope("CustomerApi", "Customer API"),
+                    new ApiScope("AdminApi", "Admin API")
+               };
+
+          public static IEnumerable<Client> Clients =>
+               new List<Client>
+               {
+               };
+     }
+}
