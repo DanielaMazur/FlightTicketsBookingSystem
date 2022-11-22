bf4f281a94ccc1dafd9181d11d02989d194e3b05
Mazur Daniela <danielamazur2000@gmail.com>
2022-11-22 14:46:28 +0200

    Add ELK to Services

--- CacheService/CacheService/Program.cs
+++ CacheService/CacheService/Program.cs
@@ -1,5 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace CacheService
 {
@@ -7,14 +15,69 @@ namespace CacheService
      {
           public static void Main(string[] args)
           {
-               CreateHostBuilder(args).Build().Run();
+               //configure logging first
+               ConfigureLogging();
+
+               //then create the host, so that if the host fails we can log errors
+               CreateHost(args);
+          }
+
+          private static void CreateHost(string[] args)
+          {
+               try
+               {
+                    CreateHostBuilder(args).Build().Run();
+               }
+               catch (Exception ex)
+               {
+                    Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+                    throw;
+               }
           }
 
           public static IHostBuilder CreateHostBuilder(string[] args) =>
-              Host.CreateDefaultBuilder(args)
-                  .ConfigureWebHostDefaults(webBuilder =>
-                  {
-                       webBuilder.UseStartup<Startup>();
-                  });
+               Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                         webBuilder.UseStartup<Startup>();
+                    })
+                    .ConfigureAppConfiguration(configuration =>
+                    {
+                         configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                         configuration.AddJsonFile(
+                              $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                              optional: true);
+                    })
+                    .UseSerilog();
+
+          private static void ConfigureLogging()
+          {
+               var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+               var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile(
+                         $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                         optional: true)
+                    .Build();
+
+               Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                    .Enrich.WithProperty("Environment", environment)
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+          }
+
+          private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+          {
+               Console.WriteLine("Elastic Search URL {0}", configuration["ElasticConfiguration:Uri"]);
+               return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+               {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+               };
+          }
      }
 }
--- CustomerService/CustomerService/Program.cs
+++ CustomerService/CustomerService/Program.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace CustomerService
 {
@@ -7,14 +12,72 @@ namespace CustomerService
      {
           public static void Main(string[] args)
           {
-               CreateHostBuilder(args).Build().Run();
+               //configure logging first
+               ConfigureLogging();
+
+               //then create the host, so that if the host fails we can log errors
+               CreateHost(args);
+          }
+
+          private static void CreateHost(string[] args)
+          {
+               try
+               {
+                    CreateHostBuilder(args).Build().Run();
+               }
+               catch (Exception ex)
+               {
+                    Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+                    throw;
+               }
           }
 
           public static IHostBuilder CreateHostBuilder(string[] args) =>
-              Host.CreateDefaultBuilder(args)
-                  .ConfigureWebHostDefaults(webBuilder =>
-                  {
-                       webBuilder.UseStartup<Startup>();
-                  });
+               Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                         webBuilder.UseStartup<Startup>();
+                    })
+                    .ConfigureAppConfiguration(configuration =>
+                    {
+                         configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                         configuration.AddJsonFile(
+                              $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                              optional: true);
+                    })
+                    .UseSerilog();
+
+          private static void ConfigureLogging()
+          {
+               var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+               var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile(
+                         $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                         optional: true)
+                    .Build();
+
+               Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Debug()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                    .Enrich.WithProperty("Environment", environment)
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+          }
+
+          private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+          {
+               Console.WriteLine("Elastic Search URL {0}", configuration["ElasticConfiguration:Uri"]);
+               return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+               {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+               };
+          }
      }
 }
+
+
